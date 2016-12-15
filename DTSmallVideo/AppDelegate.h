06fc63c7046781cfc00ef2046481d4cb3769a644
cuiquan <cuiquan1987@163.com>
2016-12-15 11:30:08 +0800

    Initial Commit

---
+++ DTSmallVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DTSmallVideo
+//
+//  Created by quan cui on 2016/12/15.
+//  Copyright © 2016年 quan cui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DTSmallVideo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DTSmallVideo
+//
+//  Created by quan cui on 2016/12/15.
+//  Copyright © 2016年 quan cui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
